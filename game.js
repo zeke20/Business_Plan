5faaf5ab1e93fd519da258221621c90766dd48e6
zeke20 <ezekielfranklin12@gmail.com>
2023-10-17 16:05:41 -0400

    test

--- game.js
+++ game.js
@@ -27,7 +27,7 @@ canvas.width = 800;
 canvas.height = 600;
 
 const objectsToFind = [
-    new ObjectToFind("Key", 375, 520, 50, 50), // Replace with your image-specific objects
+    new ObjectToFind("Key", 370, 550, 20, 20), // Replace with your image-specific objects
     // Add more objects for other images here
 ];
 
